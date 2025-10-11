98172a42bdba533028aa87571e747e8fb6f260bf
mangchohyeon <qgeon0419@gmail.com>
2025-10-11 22:59:51 +0900

    tsconfig path 관련 설정함.

--- app.config.ts
+++ app.config.ts
@@ -1,3 +1,8 @@
 import { defineConfig } from "@solidjs/start/config";
+import tsconfigPath from "vite-tsconfig-paths";
 
-export default defineConfig({});
+export default defineConfig({
+    vite: {
+        plugins: [tsconfigPath()]
+    }
+});
