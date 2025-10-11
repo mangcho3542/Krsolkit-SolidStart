9813223500855f47dd50c23e34428778869cc68d
qgeon0419 <qgeon0419@gmail.com>
2025-10-11 04:42:06 +0000

    app.tsx 수정함.

--- src/app.tsx
+++ src/app.tsx
@@ -3,16 +3,18 @@ import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
 import "./app.css";
+import DefaultLayout from "@layout/DefaultLayout";
 
 export default function App() {
   return (
     <Router
       root={props => (
         <MetaProvider>
-          <Title>SolidStart - Basic</Title>
-          <a href="/">Index</a>
-          <a href="/about">About</a>
-          <Suspense>{props.children}</Suspense>
+          <Title>ClassHelper</Title>
+          
+          <DefaultLayout>
+            <Suspense>{props.children}</Suspense>
+          </DefaultLayout>
         </MetaProvider>
       )}
     >
