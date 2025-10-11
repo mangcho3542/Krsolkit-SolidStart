5af13c1ece5af1b6af066b7aa5bf87004b8508b8
mangchohyeon <qgeon0419@gmail.com>
2025-10-11 23:00:06 +0900

    A 컴포넌트 만듬.

---
+++ src/components/A.tsx
@@ -0,0 +1,13 @@
+import { A as Anchor, AnchorProps as AProps } from "@solidjs/router";
+import styles from '@styles/A.module.css';
+
+export interface AnchorProps extends AProps {};
+
+export default function A({children, class:className="", ...rest}: AProps) {
+
+    return (
+        <Anchor class={styles.A + className} {...rest}>
+            {children}
+        </Anchor>
+    )
+}
