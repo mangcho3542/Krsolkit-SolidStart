b447c8818fc892a048fae0fd37d7edc53f8fe68d
mangchohyeon <qgeon0419@gmail.com>
2025-10-11 23:01:23 +0900

    index 경로 컴포넌트 만듬.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,13 @@
 import { Title } from "@solidjs/meta";
-import Counter from "~/components/Counter";
+import Text from "@components/Text";
 
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
-      <Counter />
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
+      <Title>ClassHelper</Title>
+      <Text fontSize={500}>
+        Home
+      </Text>
     </main>
   );
 }
