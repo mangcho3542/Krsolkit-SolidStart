2f603a70b5366fd01e48a0a39b060a66a640a3b1
qgeon0419 <qgeon0419@gmail.com>
2025-10-13 01:52:58 +0000

    Layout 완성

--- src/layout/DefaultLayout.tsx
+++ src/layout/DefaultLayout.tsx
@@ -2,21 +2,35 @@ import { JSX } from "solid-js";
 import styles from "./Layout.module.css";
 import Text from "@components/Text";
 import Logo from "@images/Logo.svg";
+import A from "@components/A";
 
 interface LayoutProps {
     children: JSX.Element;
 }
 
 export default function DefaultLayout({children}: LayoutProps) {
+    const sideMenuAry = ["블로그", "가이드", "로그인"];
+    const sideMenuLinks = ["/blog", "/guide", "/login"];
+
     return (
         <>
-        <nav class={styles.Nav}>
-            <div class={styles.LogoWrapper}>
-                <img class={styles.Logo} src={Logo} />
-                <Text class={styles.LogoText}>
+        <nav id={styles.Nav}>
+            <div id={styles.LogoWrapper}>
+                <img id={styles.Logo} src={Logo} />
+                <Text id={styles.LogoText}>
                     ClassHelper
                 </Text>
             </div>
+
+            <div id={styles.SideMenuWrapper}>
+                {sideMenuAry.map((sideMenu, index) => (
+                    <A href={sideMenuLinks[index]} class={styles.SideMenu}>
+                        {sideMenu}
+                    </A>
+                ))}
+            </div>
+
+
         </nav>
 
         {children}
