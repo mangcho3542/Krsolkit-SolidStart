a0090b8eb94cd026fdd4703386944b5a04fdb7da
qgeon0419 <qgeon0419@gmail.com>
2025-10-13 12:55:24 +0000

    /경로 수정중

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,21 @@
 import { Title } from "@solidjs/meta";
 import Text from "@components/Text";
+import styles from "./index.module.css";
 
 export default function Home() {
   return (
-    <main>
+    <main id={styles.Main}>
       <Title>ClassHelper</Title>
-      <Text fontSize={500}>
-        Home
+
+      <Text id={styles.MainText}>
+        ClassHelper
       </Text>
+
+      <Text display="block" fontSize="2.5rem" paddingLeft="2%" border="1px solid black">
+        고등 영단어 학습을 위한 플랫폼
+      </Text>
+
+
     </main>
   );
 }
