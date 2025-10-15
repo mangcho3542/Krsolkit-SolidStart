2d23704cebdf560bc40d1188afebb927eb4dbf3c
qgeon0419 <qgeon0419@gmail.com>
2025-10-15 05:53:55 +0000

    로그인 페이지 만드는 중

---
+++ src/routes/login/index.tsx
@@ -0,0 +1,11 @@
+import styles from "./login.module.css";
+
+export default function Login() {
+    return (
+        <main class="Main">
+            <div id={styles.Wrapper}>
+                
+            </div>
+        </main>
+    )
+}
