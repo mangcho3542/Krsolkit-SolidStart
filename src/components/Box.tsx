f74225c9498ae592b8af6bb425db509ec28b6e03
qgeon0419 <qgeon0419@gmail.com>
2025-10-16 07:46:05 +0000

    Box(div) 컴포넌트 만듬.

---
+++ src/components/Box.tsx
@@ -0,0 +1,12 @@
+import { ComponentProps } from "@/types/ComponentProps";
+import { splitProps } from "@utils/splitProps";
+
+export default function Box(props: ComponentProps) {
+    const [local, style, rest] = splitProps(props, ["children"]);
+
+    return (
+        <div {...style} {...rest}>
+            {local.children}
+        </div>
+    )
+}
