b7967e9a5ea1b86566963287355de9b92ebf9aca
qgeon0419 <qgeon0419@gmail.com>
2025-10-23 03:57:08 +0000

    splitAry 함수 생성함.

---
+++ src/utils/array.ts
@@ -0,0 +1,17 @@
+type Length<T extends readonly any[]> = T['length'];
+type RangeType<N extends number, Acc extends number[] = []> = 
+  Acc['length'] extends N ? Acc[number] : RangeType<N, [...Acc, Acc['length']]>;
+
+export function splitAry<T extends any[], Indices extends RangeType<Length<T>>[]>(
+    ary: [...T],
+    indices: Indices
+) {
+    let res: T[] = [];
+
+    for(let i = 0; i < indices.length; i++) {
+        let index = indices[i];
+        res[i] = Number.isInteger(index) ? ary[index] : ary[Math.trunc(index)];
+    }
+
+    return res;
+}
