ab4d60a78cdced879640ed4b426409267197512a
mangchohyeon <qgeon0419@gmail.com>
2025-10-24 18:53:47 +0900

    ColorSwatch 컴포넌트 완성함.

--- src/components/ColorSwatch.tsx
+++ src/components/ColorSwatch.tsx
@@ -1,26 +1,43 @@
 import { ComponentProps } from "@/types/ComponentProps";
 import { Dynamic } from "solid-js/web";
 import { Hex, RGB, RGBA, HSL, HSLA } from "@/types/ColorType";
-import { JSX, ValidComponent } from "solid-js";
+import { JSX, ValidComponent, mergeProps } from "solid-js";
 import { splitProps } from "@/utils/splitProps";
 import styles from "@styles/ColorSwatch.module.css";
+import { omit } from "@/utils/object";
 
-interface ColorSwatchProps extends Pick<ComponentProps, "class" | "classList" | "id"> {
-    color: Hex | RGB | RGBA | HSL | HSLA;
-    as?: ValidComponent | keyof JSX.IntrinsicElements;
-    useDefaultStyle?: boolean;
+export interface ColorSwatchProps
+  extends Pick<ComponentProps, "class" | "classList" | "id"> {
+  color: Hex | RGB | RGBA | HSL | HSLA;
+  as?: ValidComponent | keyof JSX.IntrinsicElements;
+  useDefaultStyle?: boolean;
 }
 
-function ColorSwatch(props: ColorSwatchProps) {
-    const [local, style, rest] = splitProps(props, styles.ColorSwatch, ["color", "as", "useDefaultStyle"]);
+export function ColorSwatch(props: ColorSwatchProps) {
+  const [local, styling, rest] = splitProps(props, styles.ColorSwatch, [
+    "color",
+    "as",
+    "useDefaultStyle",
+  ]);
 
-    return (
-        <Dynamic 
-        component={local.as ?? "div"}
-        class={style.class + local.useDefaultStyle ? " " + styles.ColorSwatch : ""}
-        classList={{
-            ...style.classList,
-        }}
-        />
-    )
+  const style = {
+    ...omit(styling.style, ["background-color"]),
+    "background-color": local.color,
+  };
+
+  return (
+    <Dynamic
+      component={local.as ?? "div"}
+      class={
+        styling.class + local.useDefaultStyle ? " " + styles.ColorSwatch : ""
+      }
+      classList={{
+        ...styling.classList,
+      }}
+      style={style}
+      {...rest}
+    />
+  );
 }
+
+export default ColorSwatch;
