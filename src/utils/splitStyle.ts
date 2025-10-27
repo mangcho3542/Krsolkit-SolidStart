9499cacdb85021848ccc453abd52886bac014140
qgeon0419 <qgeon0419@gmail.com>
2025-10-27 00:25:47 +0000

    splitStyle 함수 수정함.

--- src/utils/splitStyle.ts
+++ src/utils/splitStyle.ts
@@ -1,18 +1,25 @@
 import { ComponentProps } from "@/types/ComponentProps";
 import { omit } from "./object";
+import { JSX, mergeProps } from "solid-js";
 
 interface SplitStyleProps extends StyleI {
   useDefaultStyle?: boolean;
   [k: string]: any;
 }
 
-interface StyleI extends Pick<ComponentProps, "class" | "classList" | "id"> {}
+interface StyleI extends Pick<ComponentProps, "class" | "classList" | "id"> {
+  style?: JSX.CSSProperties;
+}
 
 export function splitStyle(props: SplitStyleProps, style: StyleI) {
-  if (props.useDefaultStyle !== undefined && props.useDefaultStyle)
-    return {
-      ...omit(props, ["useDefaultStyle", "class"]),
-      ...style,
-    };
+  if (props.useDefaultStyle !== undefined && props.useDefaultStyle) {
+    const obj = omit(props, ["useDefaultStyle"]);
+    obj.classList = {
+      ...(style.class ? {[style.class]: true}: {}),
+      ...style.classList
+    }
+    obj.style = mergeProps(props.style, style.style);
+    return obj;
+  }  
   else return omit(props, ["useDefaultStyle"]);
 }
