a9633f761036226e8c69a4442eb02981c23ca3a1
qgeon0419 <qgeon0419@gmail.com>
2025-10-27 03:29:46 +0000

    Dialog컴포넌트 완성함.

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -1,27 +1,72 @@
 import { splitProps } from "solid-js";
-import { SvgProps, Svg } from "./Svg";
-import CloseIcon from "@images/CloseIcon.svg";
+import {
+  Dialog as ArkDialog,
+  DialogRootProps,
+  DialogTriggerProps,
+  DialogPositionerProps,
+  DialogContentProps,
+  DialogTitleProps,
+  DialogDescriptionProps,
+  DialogCloseTriggerProps,
+} from "@ark-ui/solid";
+import { Portal } from "solid-js/web";
+import CloseBtn from "./CloseBtn";
+import { splitStyle } from "@/utils/splitStyle";
 import styles from "@styles/Dialog.module.css";
 
-interface CloseIconProps extends Omit<SvgProps, "src"> {
-  useDefaultStyle?: boolean;
+interface ContentProps extends DialogContentProps {useDefaultStyle?: boolean;}
+interface TitleProps extends DialogTitleProps {useDefaultStyle?: boolean;}
+interface DescriptionProps extends DialogDescriptionProps {useDefaultStyle?: boolean};
+
+export interface DialogProps extends DialogRootProps {
+  TriggerProps?: DialogTriggerProps;
+  PositionerProps?: DialogPositionerProps;
+  ContentProps?: ContentProps;
+  TitleProps?: TitleProps;
+  Title?: Element;
+  DescriptionProps?: DescriptionProps;
+  Description?: Element;
+  CloseTriggerProps?: DialogCloseTriggerProps;
+  CloseTrigger?: Element;
 }
 
-function CloseBtn(props: CloseIconProps) {
-  const [local, rest] = splitProps(props, ["class", "useDefaultStyle"]);
+export function Dialog(props: DialogProps) {
+  const [local, rest] = splitProps(props, [
+    "TriggerProps",
+    "PositionerProps",
+    "ContentProps",
+    "TitleProps",
+    "Title",
+    "DescriptionProps",
+    "Description",
+    "CloseTriggerProps",
+    "CloseTrigger",
+  ]);
 
   return (
-    <Svg
-      src={CloseIcon}
-      class={
-        local.class ??
-        " " + (local.useDefaultStyle !== undefined && local.useDefaultStyle)
-          ? styles.CloseBtn
-          : ""
-      }
-      {...rest}
-    />
+    <ArkDialog.Root {...rest}>
+      <ArkDialog.Trigger {...local.TriggerProps} />
+
+      <Portal>
+        <ArkDialog.Backdrop />
+        <ArkDialog.Positioner {...local.PositionerProps}>
+          <ArkDialog.Content {...splitStyle(local.ContentProps, {class: styles.DialogContent})}>
+            <ArkDialog.Title {...local.TitleProps}>
+              {local.Title}
+            </ArkDialog.Title>
+
+            <ArkDialog.Description {...local.DescriptionProps}>
+              {local.Description}
+            </ArkDialog.Description>
+
+            <ArkDialog.CloseTrigger {...local.CloseTriggerProps}>
+              {local.CloseTrigger ?? <CloseBtn />}
+            </ArkDialog.CloseTrigger>
+          </ArkDialog.Content>
+        </ArkDialog.Positioner>
+      </Portal>
+    </ArkDialog.Root>
   );
 }
 
-function Dialog() {}
+export default Dialog;
