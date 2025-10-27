ec9de08b724ffc5e30507788d0429f58be2c7997
qgeon0419 <qgeon0419@gmail.com>
2025-10-27 13:02:05 +0000

    test코드 짜는 중

--- src/routes/_test/index.tsx
+++ src/routes/_test/index.tsx
@@ -1,16 +1,22 @@
-import { Portal } from "solid-js/web";
+import Dialog from "@/components/Dialog";
 import styles from "./test.module.css";
+import Btn from "@components/Btn";
+import { createSignal } from "solid-js";
 
 export default function test() {
-    return (
-        <main class="Main">
-            <Portal 
-            mount={document.getElementsByClassName("Main")[0]}
-            >
-                <div class={styles.Portal}>
-                    포털
-                </div>
-            </Portal>
-        </main>
-    )
+  const [open, setOpen] = createSignal<bool>(false);
+
+  return (
+    <main class="Main">
+      <div id={styles.BtnWrapper}>
+        <Btn id={styles.TrgBtn}>
+          Open Dialog
+        </Btn>
+      </div>
+
+      <Dialog class={styles.DialogRoot}
+
+        />
+    </main>
+  );
 }
