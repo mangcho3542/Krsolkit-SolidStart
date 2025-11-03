0b3d60d84dcf72b92c039ac52e1daea67303f8f5
qgeon0419 <qgeon0419@gmail.com>
2025-11-03 09:15:01 +0000

    slider 테스트함.

--- src/routes/_test/index.tsx
+++ src/routes/_test/index.tsx
@@ -1,38 +1,14 @@
-import Dialog from "@/components/Dialog";
+import Slider from "@components/Slider";
 import styles from "./test.module.css";
-import Btn from "@components/Btn";
-import { createEffect, createSignal, onCleanup } from "solid-js";
 
 export default function test() {
-  const [TrgRef, setTrgRef] = createSignal<HTMLButtonElement | undefined>(undefined);
-
-  createEffect(() => {
-    if(!TrgRef()) return;
-    function handler() {console.log("클릭됨\nTrgRef\n", TrgRef());}
-
-    TrgRef()!.addEventListener("click", handler);
-    onCleanup(() => {
-      TrgRef()!.removeEventListener("click", handler);
-    })
-  });
-
   return (
-    <main class="Main" id={styles.Main}>
-      <div id={styles.BtnWrapper}>
-        <Btn id={styles.TrgBtn} ref={setTrgRef}>
-          Open Dialog
-        </Btn>
+    <div id={styles.Main}>
+      <div id={styles.SliderWrapper}>
+        <Slider 
+        Label="라벨"
+        />
       </div>
-
-      <Dialog
-        id={styles.DialogRoot}
-        Title="타이틀"
-        DescProps={{ id: styles.DialogDesc }}
-        Desc="설명"
-        ContentProps={{ id: styles.DialogContent }}
-        Content="콘텐트"
-        TrgRef={TrgRef()}
-      />
-    </main>
+    </div>
   );
 }
