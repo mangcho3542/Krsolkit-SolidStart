95853149da1bd873429833dd420fd320b4d565b0
qgeon0419 <qgeon0419@gmail.com>
2025-11-05 03:56:25 +0000

    splitComponentProps utils함수 수정함.

--- src/utils/splitComponentProps.ts
+++ src/utils/splitComponentProps.ts
@@ -1,53 +1,25 @@
-// splitComponentProps.ts
-import {
-  ComponentBaseProps,
-  ComponentBaseStyleProps,
-} from "@/types/ComponentProps";
-import { splitProps } from "solid-js";
-import { JSX } from "solid-js";
-import { convertCss } from "./converCss";
+import { ComponentBaseStyleProps } from "@/types/ComponentProps";
 
-interface BaseProps extends ComponentBaseStyleProps {
-  useDefaultStyle?: boolean;
-  stlye?: JSX.CSSProperties;
-  [k: string]: any;
-}
-
-interface BaseStyleProps extends ComponentBaseProps {
+interface I extends ComponentBaseStyleProps {
   useDefaultStyle?: boolean;
   [k: string]: any;
 }
 
-/**
- * 반환값은 원본을 건드리지 않는 새 객체입니다.
- * 기본 클래스(defaultClass)를 기존 class/className/classList에 병합하여 `class` 문자열로 만든 뒤 반환.
- */
-export function splitComponentProps<T extends BaseProps>(
-  props?: T,
-  defaultClass?: string
-): ComponentBaseStyleProps {
-  if (!props) return { class: defaultClass } as ComponentBaseProps;
-  const [res, rest] = splitProps(props, [
-    "class",
-    "id",
-    "clasList",
-    "children",
-  ]) as [
-    ComponentBaseStyleProps,
-    Omit<BaseStyleProps, "class" | "id" | "classList" | "children">
-  ];
+export function splitComponentProps<T extends I>(
+  props: I | undefined,
+  defaultClass: string
+): typeof props {
+  if(!props) return {};
 
-  res.style = convertCss(rest.css);
-  if (rest.useDefaultStyle === undefined)
-    res.class += " " + (defaultClass ?? "");
-  else {
-    res.class += " " + (rest.useDefaultStyle ? defaultClass ?? "" : "");
-  }
+  const { useDefaultStyle = true, class: cls, ...rest } = props;
 
-  delete rest.css;
-  delete rest.useDefaultStyle;
+  const combined = [cls, useDefaultStyle ? defaultClass : ""]
+    .filter(Boolean)
+    .join(" ")
+    .trim();
 
-  return {...res, ...rest};
+  return {
+    ...rest,
+    class: combined,
+  } as T;
 }
-
-export default splitComponentProps;
