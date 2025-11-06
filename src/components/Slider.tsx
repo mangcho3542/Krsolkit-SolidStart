8140aaf8a737d114e29ebcf1bddf3c7b42517708
qgeon0419 <qgeon0419@gmail.com>
2025-11-06 01:32:06 +0000

    예전 버전으로 Slider를 되돌림.

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,57 +1,168 @@
+import {
+  Slider as ArkSlider,
+  SliderRootProps,
+  SliderLabelProps as ArkSliderLabelProps,
+  SliderValueTextProps as ArkSliderValueTextProps,
+  SliderControlProps as ArkSliderControlProps,
+  SliderTrackProps as ArkSliderTrackProps,
+  SliderRangeProps as ArkSliderRangeProps,
+  SliderThumbProps as ArkSliderThumbProps,
+} from "@ark-ui/solid/slider";
+import { JSXElement, splitProps, JSX } from "solid-js";
 import styles from "@styles/Slider.module.css";
-import { ComponentBaseStyleProps } from "@/types/ComponentProps";
+import { CssProperties } from "@/types/CssProperties";
 import { splitComponentProps } from "@/utils/splitComponentProps";
-import { createMemo, createSignal, JSXElement, splitProps } from "solid-js";
 
-interface PUD extends ComponentBaseStyleProps {
+interface SliderLabelProps extends Omit<ArkSliderLabelProps, "style"> {
+  useDefaultStyle?: boolean;
+  style?: JSX.CSSProperties;
+}
+interface SliderValueTextProps extends Omit<ArkSliderValueTextProps, "style"> {
+  useDefaultStyle?: boolean;
+  style?: JSX.CSSProperties;
+}
+interface SliderControlProps extends Omit<ArkSliderControlProps, "style"> {
+  useDefaultStyle?: boolean;
+  style?: JSX.CSSProperties;
+}
+interface SliderTrackProps extends Omit<ArkSliderTrackProps, "style"> {
+  useDefaultStyle?: boolean;
+  style?: JSX.CSSProperties;
+}
+interface SliderRangeProps extends Omit<ArkSliderRangeProps, "style"> {
+  useDefaultStyle?: boolean;
+  style?: JSX.CSSProperties;
+}
+interface SliderThumbProps extends Omit<ArkSliderThumbProps, "style"> {
+  useDefaultStyle?: boolean;
+  style?: JSX.CSSProperties;
+}
+
+export interface SliderProps extends Omit<SliderRootProps, "defaultValue"> {
+  LabelProps?: SliderLabelProps;
+  Label?: JSXElement;
+  ValueTextProps?: SliderValueTextProps;
+  ControlProps?: SliderControlProps;
+  TrackProps?: SliderTrackProps;
+  RangeProps?: SliderRangeProps;
+  ThumbProps?: Omit<SliderThumbProps, "index">;
+  defaultValue?: number;
+  useDefaultStyle?: boolean;
+  css?: CssProperties;
+}
+
+interface Obj {
+  class?: string;
+  classList?: { [k: string]: boolean | undefined };
+  id?: string;
   useDefaultStyle?: boolean;
 }
 
-interface SliderProps extends PUD {
-  LabelWrapperProps?: PUD;
-  LabelProps?: PUD;
-  Label?: JSXElement;
-  ValueTextProps?: PUD;
-  RangeProps?: PUD;
-  ThumbProps?: PUD;
-  TrackProps?: PUD;
-  defaultValue?: number;
-  min?: number;
-  max?: number;
-  step?: number;
-}
-
-export function Slider(props: SliderProps) {
-  const [local, other, rest] = splitProps(
-    props,
-    ["defaultValue", "min", "max", "step"],
+export function Slider(p: SliderProps) {
+  const [local, style, other, rest] = splitProps(
+    p,
     [
-      "LabelWrapperProps",
+      "useDefaultStyle",
+      "aria-label",
+      "aria-labelledby",
+      "asChild",
+      "defaultValue",
+      "disabled",
+      "form",
+      "getAriaValueText",
+      "css",
+      "ids",
+      "invalid",
+      "max",
+      "min",
+      "minStepsBetweenThumbs",
+      "name",
+      "onFocusChange",
+      "onValueChange",
+      "onValueChangeEnd",
+      "orientation",
+      "origin",
+      "readOnly",
+      "step",
+      "thumbAlignment",
+      "thumbSize",
+      "value",
+    ],
+    ["class", "id", "classList"],
+    [
       "LabelProps",
       "Label",
       "ValueTextProps",
+      "ControlProps",
+      "TrackProps",
       "RangeProps",
       "ThumbProps",
-      "TrackProps",
     ]
   );
 
-  const min = createMemo(() => local.min ?? 0);
-  const max = createMemo(() => local.max ?? 0);
-
   return (
-    <div {...splitComponentProps(rest, styles.Root)}>
-      <div
-        {...splitComponentProps(other.LabelWrapperProps, styles.LabelWrapper)}
-      >
-        {other.Label && (
-          <label {...splitComponentProps(other.LabelProps, styles.Label)}>
-            {other.Label}
-          </label>
-        )}
+    <ArkSlider.Root
+      {...splitComponentProps(style, styles.Root)}
+      {...rest}
+      aria-label={local["aria-label"]}
+      aria-labelledby={local["aria-labelledby"]}
+      asChild={local.asChild}
+      defaultValue={
+        local.defaultValue !== undefined
+          ? [local.defaultValue]
+          : [local.min ?? 0]
+      }
+      disabled={local.disabled}
+      form={local.form}
+      getAriaValueText={local.getAriaValueText}
+      ids={local.ids}
+      invalid={local.invalid}
+      max={local.max ?? 100}
+      min={local.min ?? 0}
+      minStepsBetweenThumbs={local.minStepsBetweenThumbs}
+      name={local.name}
+      onFocusChange={local.onFocusChange}
+      onValueChange={local.onValueChange}
+      onValueChangeEnd={local.onValueChangeEnd}
+      orientation={local.orientation}
+      origin={local.origin}
+      readOnly={local.readOnly}
+      step={local.step}
+      thumbAlignment={local.thumbAlignment}
+      thumbSize={local.thumbSize}
+      value={local.value}
+    >
+      <div style={{ display: "inline-block", width: "100%" }}>
+        <ArkSlider.Label
+          {...splitComponentProps(other.LabelProps, styles.Label)}
+        >
+          {other.Label}
+        </ArkSlider.Label>
 
-        {}
+        <ArkSlider.ValueText
+          {...splitComponentProps(other.ValueTextProps, styles.ValueText)}
+        />
       </div>
-    </div>
+
+      <ArkSlider.Control
+        {...splitComponentProps(other.ControlProps, styles.Control)}
+      >
+        <ArkSlider.Track
+          {...splitComponentProps(other.TrackProps, styles.Track)}
+        >
+          <ArkSlider.Range
+            {...splitComponentProps(other.RangeProps, styles.Range)}
+          />
+        </ArkSlider.Track>
+        <ArkSlider.Thumb
+          {...splitComponentProps(other.ThumbProps, styles.Thumb)}
+          index={0}
+        >
+          <ArkSlider.HiddenInput />
+        </ArkSlider.Thumb>
+      </ArkSlider.Control>
+    </ArkSlider.Root>
   );
 }
+
+export default Slider;
