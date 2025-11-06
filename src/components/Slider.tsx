b522c5d4089faab36febfa29254d16183b79dc20
qgeon0419 <qgeon0419@gmail.com>
2025-11-06 07:09:06 +0000

    Slider 스타일 수정함.

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -10,9 +10,13 @@ import {
 } from "@ark-ui/solid/slider";
 import { JSXElement, splitProps, JSX } from "solid-js";
 import styles from "@styles/Slider.module.css";
-import { CssProperties } from "@/types/CssProperties";
 import { splitComponentProps } from "@/utils/splitComponentProps";
+import { ComponentBaseStyleProps } from "@/types/ComponentProps";
 
+interface SliderLabelWrapperProps extends ComponentBaseStyleProps {
+  useDefaultStyle?: boolean;
+  style?: JSX.CSSProperties;
+}
 interface SliderLabelProps extends Omit<ArkSliderLabelProps, "style"> {
   useDefaultStyle?: boolean;
   style?: JSX.CSSProperties;
@@ -38,7 +42,8 @@ interface SliderThumbProps extends Omit<ArkSliderThumbProps, "style"> {
   style?: JSX.CSSProperties;
 }
 
-export interface SliderProps extends Omit<SliderRootProps, "defaultValue"> {
+export interface SliderProps extends Omit<SliderRootProps, "style"> {
+  LabelWrapperProps?: SliderLabelWrapperProps;
   LabelProps?: SliderLabelProps;
   Label?: JSXElement;
   ValueTextProps?: SliderValueTextProps;
@@ -46,50 +51,15 @@ export interface SliderProps extends Omit<SliderRootProps, "defaultValue"> {
   TrackProps?: SliderTrackProps;
   RangeProps?: SliderRangeProps;
   ThumbProps?: Omit<SliderThumbProps, "index">;
-  defaultValue?: number;
   useDefaultStyle?: boolean;
-  css?: CssProperties;
+  style?: JSX.CSSProperties;
 }
 
-interface Obj {
-  class?: string;
-  classList?: { [k: string]: boolean | undefined };
-  id?: string;
-  useDefaultStyle?: boolean;
-}
-
-export function Slider(p: SliderProps) {
-  const [local, style, other, rest] = splitProps(
-    p,
+export function Slider(props: SliderProps) {
+  const [other, style, rest] = splitProps(
+    props,
     [
-      "useDefaultStyle",
-      "aria-label",
-      "aria-labelledby",
-      "asChild",
-      "defaultValue",
-      "disabled",
-      "form",
-      "getAriaValueText",
-      "css",
-      "ids",
-      "invalid",
-      "max",
-      "min",
-      "minStepsBetweenThumbs",
-      "name",
-      "onFocusChange",
-      "onValueChange",
-      "onValueChangeEnd",
-      "orientation",
-      "origin",
-      "readOnly",
-      "step",
-      "thumbAlignment",
-      "thumbSize",
-      "value",
-    ],
-    ["class", "id", "classList"],
-    [
+      "LabelWrapperProps",
       "LabelProps",
       "Label",
       "ValueTextProps",
@@ -97,67 +67,29 @@ export function Slider(p: SliderProps) {
       "TrackProps",
       "RangeProps",
       "ThumbProps",
-    ]
+    ],
+    ["class", "id", "classList", "style", "useDefaultStyle"]
   );
 
   return (
-    <ArkSlider.Root
-      {...splitComponentProps(style, styles.Root)}
-      {...rest}
-      aria-label={local["aria-label"]}
-      aria-labelledby={local["aria-labelledby"]}
-      asChild={local.asChild}
-      defaultValue={
-        local.defaultValue !== undefined
-          ? [local.defaultValue]
-          : [local.min ?? 0]
-      }
-      disabled={local.disabled}
-      form={local.form}
-      getAriaValueText={local.getAriaValueText}
-      ids={local.ids}
-      invalid={local.invalid}
-      max={local.max ?? 100}
-      min={local.min ?? 0}
-      minStepsBetweenThumbs={local.minStepsBetweenThumbs}
-      name={local.name}
-      onFocusChange={local.onFocusChange}
-      onValueChange={local.onValueChange}
-      onValueChangeEnd={local.onValueChangeEnd}
-      orientation={local.orientation}
-      origin={local.origin}
-      readOnly={local.readOnly}
-      step={local.step}
-      thumbAlignment={local.thumbAlignment}
-      thumbSize={local.thumbSize}
-      value={local.value}
-    >
-      <div style={{ display: "inline-block", width: "100%" }}>
+    <ArkSlider.Root {...splitComponentProps(style, styles.Root)}>
+      <div
+        {...splitComponentProps(other.LabelWrapperProps, styles.LabelWrapper)}
+      >
         <ArkSlider.Label
           {...splitComponentProps(other.LabelProps, styles.Label)}
         >
           {other.Label}
         </ArkSlider.Label>
-
-        <ArkSlider.ValueText
-          {...splitComponentProps(other.ValueTextProps, styles.ValueText)}
-        />
+        <ArkSlider.ValueText {...splitComponentProps(other.ValueTextProps, styles.ValueText)} />
       </div>
 
-      <ArkSlider.Control
-        {...splitComponentProps(other.ControlProps, styles.Control)}
-      >
-        <ArkSlider.Track
-          {...splitComponentProps(other.TrackProps, styles.Track)}
-        >
-          <ArkSlider.Range
-            {...splitComponentProps(other.RangeProps, styles.Range)}
-          />
+      <ArkSlider.Control {...splitComponentProps(other.ControlProps, styles.Control)}>
+        <ArkSlider.Track {...splitComponentProps(other.TrackProps, styles.Track)}>
+          <ArkSlider.Range {...splitComponentProps(other.RangeProps, styles.Range)} />
         </ArkSlider.Track>
-        <ArkSlider.Thumb
-          {...splitComponentProps(other.ThumbProps, styles.Thumb)}
-          index={0}
-        >
+
+        <ArkSlider.Thumb index={0} {...splitComponentProps(other.ThumbProps, styles.Thumb)}>
           <ArkSlider.HiddenInput />
         </ArkSlider.Thumb>
       </ArkSlider.Control>
