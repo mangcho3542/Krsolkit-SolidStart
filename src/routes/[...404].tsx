10819b08a3731618dff40032e0bed994bc426e1c
mangchohyeon <qgeon0419@gmail.com>
2025-11-06 22:15:36 +0900

    404 페이지 만듬.

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -1,19 +1,37 @@
 import { Title } from "@solidjs/meta";
 import { HttpStatusCode } from "@solidjs/start";
+import styles from "@styles/404.module.css";
+import A from "@components/A";
 
 export default function NotFound() {
   return (
-    <main>
+    <main class="Main" style={{ "align-items": "center" }}>
       <Title>Not Found</Title>
       <HttpStatusCode code={404} />
-      <h1>Page Not Found</h1>
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
+      <h1
+        style={{
+          display: "block",
+          "font-family": "SuitSemiBold, var(--default-font-family)",
+          "font-size": "1.6rem",
+        }}
+      >
+        존재하지 않는 페이지
+      </h1>
+      <p class={styles.Text}>
+        <h2>페이지를 찾을 수 없습니다!</h2> <br />
+        요청하신 페이지가 삭제되었거나 주소가 변경되었을 수도 있습니다. 다시
+        확인해 주십시오&#128522;
       </p>
+      <br />
+      <A
+        href="/"
+        style={{
+          "font-family": "SuitSemiBold, var(--default-font-family)",
+          "font-size": "1.3rem",
+        }}
+      >
+        홈으로 가기
+      </A>
     </main>
   );
 }
