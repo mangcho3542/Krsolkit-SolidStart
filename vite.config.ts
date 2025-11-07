49bdf4fe7411e7a527d315305b821b6d9f833ca7
qgeon0419 <qgeon0419@gmail.com>
2025-11-07 01:44:42 +0000

    vite.config.ts에서 cloudflare설정함.

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import solid from "solid-start/vite";
+import cloudflare from "solid-start-cloudflare-pages";
+
+export default defineConfig({
+  plugins: [
+    solid({
+      adapter: cloudflare({}),
+    }),
+  ],
+});
