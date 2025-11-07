c5dbf878874f3222fdd4d299cfa90531f01039bd
qgeon0419 <qgeon0419@gmail.com>
2025-11-07 13:31:40 +0000

    로그인 페이지 만드는 중

--- src/routes/login/index.tsx
+++ src/routes/login/index.tsx
@@ -1,14 +1,14 @@
+import { Title } from "@solidjs/meta";
 import styles from "./login.module.css";
-import { Title, Link } from "@solidjs/meta";
 
 export default function Login() {
   return (
     <>
       <Title>로그인</Title>
-      <Link rel="icon" href="/favicon.ico" />
-
       <main class="Main">
-        <div id={styles.Wrapper}></div>
+        <div id={styles.Wrapper}>
+
+        </div>
       </main>
     </>
   );
