fc038cbab65dda7786804afd4a188fad6a462c7c
mangchohyeon <qgeon0419@gmail.com>
2025-11-09 15:30:00 +0900

    signup 페이지 경로 이동함.

--- src/routes/(login)/signup/(signup).tsx
+++ src/routes/(login)/signup/(signup).tsx
@@ -6,7 +6,7 @@ export default function signUp() {
       <Title>회원가입</Title>
       <Meta name="description" content="회원가입 페이지입니다." />
       <Meta property="og:title" content="ClassHelper-회원가입" />
-
+      <Meta property="og:image" content="https://classhelper.kr/favicon.png" />
       <main class="Main"></main>
     </>
   );
