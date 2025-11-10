85968e2b1d0cca932da702e32405f8048c1840b6
qgeon0419 <qgeon0419@gmail.com>
2025-11-10 01:59:25 +0000

    PasswordInput컴포넌트에서 css관련 수정함.

--- src/components/PasswordInput.tsx
+++ src/components/PasswordInput.tsx
@@ -6,22 +6,21 @@ import {
   PasswordInputControlProps as PiControlProps,
   PasswordInputInputProps as PiInputProps,
   PasswordInputVisibilityTriggerProps as PiVisibleProps,
-  PasswordInputIndicatorProps as PiIndicatorProps
+  PasswordInputIndicatorProps as PiIndicatorProps,
 } from "@ark-ui/solid/password-input";
-import { JSX, JSXElement } from "solid-js";
+import { JSXElement } from "solid-js";
 import { splitProps } from "solid-js";
-import EyeIcon from "@images/EyeIcon.svg?raw";
-import EyeOff from "@images/EyeOff.svg?raw";
-import { SvgProps, Svg } from "./Svg";
-import { Hex } from "@/types/ColorType";
 import { splitComponentProps } from "@/utils/splitComponentProps";
-import type { PUS } from "@/types/ComponentProps";
+import type { ComponentBaseStyleProps, PUS } from "@/types/ComponentProps";
+import { NoHydration } from "solid-js/web";
 
 type LabelProps = PUS<PiLabelProps>;
 type ControlProps = PUS<PiControlProps>;
 type InputProps = PUS<PiInputProps>;
 type VisibleProps = PUS<PiVisibleProps>;
 type IndicatorProps = Omit<PUS<PiIndicatorProps>, "fallback">;
+
+interface SvgProps extends Omit<ComponentBaseStyleProps, "children"> {}
 
 type PasswordInputProps = PUS<RootProps> & {
   LabelProps?: LabelProps;
@@ -30,65 +29,95 @@ type PasswordInputProps = PUS<RootProps> & {
   InputProps?: InputProps;
   VisibleTriggerProps?: VisibleProps;
   IndicatorProps?: Omit<IndicatorProps, "fallback">;
-  EyeProps?: Omit<SvgProps, "src">;
-  EyeOffProps?: Omit<SvgProps, "src">;
-}
+  EyeProps?: SvgProps;
+  EyeOffProps?: SvgProps;
+};
 
-function EyeIconComp(props: Omit<SvgProps, "value">) {
+function EyeIconComp(props: SvgProps) {
   return (
-    <Svg
-      class={props.class}
-      classList={props.classList}
-      id={props.id}
-      color={props.color ?? ("#262628" as Hex)}
-      value={EyeIcon}
-    />
+    <NoHydration>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        width="24"
+        height="24"
+        {...splitComponentProps(props, styles.EyeIcon)}
+      >
+        <path d="M10.733 5.076a10.744 10.744 0 0 1 11.205 6.575 1 1 0 0 1 0 .696 10.747 10.747 0 0 1-1.444 2.49" />
+        <path d="M14.084 14.158a3 3 0 0 1-4.242-4.242" />
+        <path d="M17.479 17.499a10.75 10.75 0 0 1-15.417-5.151 1 1 0 0 1 0-.696 10.75 10.75 0 0 1 4.446-5.143" />
+        <path d="m2 2 20 20" />
+      </svg>
+    </NoHydration>
   );
 }
 
-function EyeOffComp(props: Omit<SvgProps, "value">) {
+function EyeOffComp(props: SvgProps) {
   return (
-    <Svg
-      class={props.class}
-      classList={props.classList}
-      id={props.id}
-      color={props.color ?? ("#262628" as Hex)}
-      value={EyeOff}
-    />
+    <NoHydration>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        width="24"
+        height="24"
+        {...splitComponentProps(props, styles.EyeOffIcon)}
+      >
+        <path d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0" />
+        <circle cx="12" cy="12" r="3" />
+      </svg>
+    </NoHydration>
   );
 }
 
 export function PasswordInput(props: PasswordInputProps) {
-  const [local, style, rest] = splitProps(props, [
-    "LabelProps",
-    "Label",
-    "ControlProps",
-    "InputProps",
-    "VisibleTriggerProps",
-    "IndicatorProps",
-    "EyeProps",
-    "EyeOffProps"
-  ], 
-  [
-    "class",
-    "id",
-    "classList",
-    "style",
-    "useDefaultStyle"
-  ]);
+  const [local, style, rest] = splitProps(
+    props,
+    [
+      "LabelProps",
+      "Label",
+      "ControlProps",
+      "InputProps",
+      "VisibleTriggerProps",
+      "IndicatorProps",
+      "EyeProps",
+      "EyeOffProps",
+    ],
+    ["class", "id", "classList", "style", "useDefaultStyle"]
+  );
 
   return (
     <PI.Root {...rest} {...splitComponentProps(style, styles.Root)}>
-      <PI.Label {...splitComponentProps(local.LabelProps, styles.Label)}>{local.Label}</PI.Label>
+      <PI.Label {...splitComponentProps(local.LabelProps, styles.Label)}>
+        {local.Label}
+      </PI.Label>
 
       <PI.Control {...splitComponentProps(local.ControlProps, styles.Control)}>
         <PI.Input {...splitComponentProps(local.InputProps, styles.Input)} />
-        <PI.VisibilityTrigger {...splitComponentProps(local.VisibleTriggerProps, styles.VisibleTrigger)}>
+        <PI.VisibilityTrigger
+          {...splitComponentProps(
+            local.VisibleTriggerProps,
+            styles.VisibleTrigger
+          )}
+        >
           <PI.Indicator
             {...splitComponentProps(local.IndicatorProps, styles.Indicator)}
-            fallback={<EyeIconComp {...splitComponentProps(local.EyeProps, styles.Eye)} />}
+            fallback={
+              <EyeIconComp
+                {...splitComponentProps(local.EyeProps, styles.EyeIcon)}
+              />
+            }
           >
-            <EyeOffComp {...splitComponentProps(local.EyeOffProps, styles.EyeOff)} />
+            <EyeOffComp {...local.EyeOffProps} />
           </PI.Indicator>
         </PI.VisibilityTrigger>
       </PI.Control>
