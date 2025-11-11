cd18a0c86554859d363453ef0c5d225732fd0f4e
mangchohyeon <qgeon0419@gmail.com>
2025-11-11 23:22:06 +0900

    account페이지 만들 예정

---
+++ src/routes/(user)/account/(account).tsx
@@ -0,0 +1,12 @@
+import { Title } from "@solidjs/meta";
+
+export default function Account() {
+    return (
+        <>
+        <Title>프로필</Title>
+        <main class="Main">
+            
+        </main>
+        </>
+    )
+}
