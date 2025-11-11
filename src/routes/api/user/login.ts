d1fd9b425e56d74ed2cf674f408da80469980420
qgeon0419 <qgeon0419@gmail.com>
2025-11-11 11:23:26 +0000

    login api는 완성함.

--- src/routes/api/user/login.ts
+++ src/routes/api/user/login.ts
@@ -1,41 +1,60 @@
 import { APIEvent } from "@solidjs/start/server";
 import axios from "axios";
+import { status } from "@/lib/status";
+import { setCookie } from "vinxi/http";
+import { redirect, json } from "@solidjs/router";
 
 interface Body {
   email: string;
   password: string;
 }
 
+interface ResponseI {
+  message?: string;
+  accessToken?: string;
+  refreshToken?: string;
+}
+
 async function handler(event: APIEvent) {
   const body = (await event.request.json()) as Body;
-  await axios
-    .post(process.env.SERVER_URL!, body, {
-      headers: { 
-        "Content-Type": "application/json",
-        "Key": process.env.SERVER_REQUEST_KEY!
-      },
-      responseType: "json"
-    })
-    .then((response) => {
-      const {status, accessToken, refreshToken} = response.data as {
-        status: boolean;
-        accessToken?: string;
-        refreshToken?: string;
-      };
-      if(status) return new Response(
-        JSON.stringify({success: true}),
-        {
-          status: 200,
-          headers: {
-            "Content-Type": "application/json",
-            //todo 이 부분부터 cookie라이브러리 사용해서 작성해야함.
-          }
-        }
-    )
-    })
-    .catch((err) => {
-      console.error("login api에서 오류 남.");
-      console.error(err);
-      process.exit(1);
+
+  const res = await axios.post(process.env.SERVER_URL!, body, {
+    headers: {
+      "Content-Type": "application/json",
+      key: process.env.SERVER_REQUEST_KEY!,
+      "Access-Control-Allow-Credential": true,
+    },
+    responseType: "json",
+    validateStatus: (status) => {
+      return status === 401;
+    }, //status가 401일 때만 정상적으로 처리
+    withCredentials: true,
+  });
+
+  //로그인 성공하면
+  if (res.status === 200) {
+    const { accessToken, refreshToken } = res.data as ResponseI;
+
+    setCookie(event.nativeEvent, "accessToken", accessToken!, {
+      httpOnly: true,
+      maxAge: 60 * 15, //15분
+      sameSite: "strict",
+      path: "/",
+      secure: process.env.NODE_ENV === "production",
     });
+
+    setCookie(event.nativeEvent, "refreshToken", refreshToken!, {
+      httpOnly: true,
+      maxAge: 60 * 60 * 24 * 30, //1달
+      sameSite: "strict",
+      path: "/",
+      secure: process.env.NODE_ENV === "production",
+    });
+
+    return redirect("/", {...status(302)});
+  }
+
+  return json(res.data, {...status(401)});
 }
+
+export const POST = handler;
