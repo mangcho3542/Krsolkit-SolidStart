5db1195e3a3eabbcd0b24622bf51c192037f1139
mangchohyeon <qgeon0419@gmail.com>
2025-11-24 21:01:27 +0900

    status lib함수 수정함.

--- src/lib/status.ts
+++ src/lib/status.ts
@@ -1,6 +1,30 @@
 import { HttpStatus } from "@/types/HttpStatus";
 import { json } from "@solidjs/router";
 
-export function status(status: keyof typeof HttpStatus, body?: object) {
-  return json(body, { status, statusText: HttpStatus[status] });
+export default function status(status: keyof typeof HttpStatus, body?: object) {
+	if (body === undefined) {
+		if (status === 500) {
+			return json(
+				{
+					success: false,
+					message:
+						"서버에서 오류가 발생하였습니다.\n나중에 다시 시도하여 주십시오.",
+				},
+				{
+					status,
+					statusText: HttpStatus[status],
+				}
+			);
+		} else if (status === 400) {
+			return json(
+				{ success: false, message: "TypeError" },
+				{
+					status,
+					statusText: HttpStatus[status],
+				}
+			);
+		}
+	}
+
+	return json(body, { status, statusText: HttpStatus[status] });
 }
