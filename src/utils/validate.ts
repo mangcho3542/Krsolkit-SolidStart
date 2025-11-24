80ecace2a7c9688bbaed059a57bd37a8ee7340e1
qgeon0419 <qgeon0419@gmail.com>
2025-11-24 06:14:31 +0000

    validate.ts에서 zod사용하도록 수정함.

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -1,16 +1,17 @@
+import { z } from "zod";
+
+const emailChecker = z.email().max(30)
+
 //regex
-const emailRegex = /^[\w.%+-]+@[\w.-]+\.[a-zA-Z]{2,}$/;
 const pwRegex = /[^\w!@#$%^&*()_+{}|:"<>?=\[\];',\.\/]/;
 
 //&email검증할 함수
-export function validateEmail(email: string) {
-  const flag = emailRegex.test(email);
-
-  return flag;
+export function validateEmail(email: string): boolean {
+  return emailChecker.safeParse(email).success;
 }
 
 //&비밀번호 검증할 함수
-export function validaetPw(pw: string) {
+export function validaetPw(pw: string): boolean {
   const flag = !pwRegex.test(pw) && pw.length >= 12 && pw.length <= 25;
 
   return flag;
