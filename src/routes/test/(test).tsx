e5ab9320c57d2f5a92bfd5abdce891eba1584527
mangcho3542 <qgeon0419@gmail.com>
2025-12-06 22:46:57 +0900

    test.tsx 수정함.

--- src/routes/test/(test).tsx
+++ src/routes/test/(test).tsx
@@ -1,26 +1,42 @@
-import Btn from "@/components/Btn";
-import { Toast, createToaster } from "@/components/Toast";
+import { Dialog } from "@components/Dialog";
 
 export default function test() {
-  const toaster = createToaster({
-    placement: "bottom-end",
-    overlap: true,
-    gap: 10,
-    duration: Infinity,
-  });
+	let btnRef: HTMLButtonElement | undefined = undefined;
 
-  return (
-    <main class="Main" style={{"align-items": "center"}}>
-      <Toast toast={toaster} />
-      <Btn onClick={() => {toaster.create({
-        title: "Title",
-        description: "Desc",
-        type: "info"
-      });
-      console.log("클릭됨.");
-      }}>
-        버튼
-      </Btn>
-    </main>
-  );
+	return (
+		<main
+			class="Main"
+			style={{
+				"align-items": "center",
+			}}
+		>
+			<div
+				style={{
+					width: "100%",
+					height: "20%",
+                    display: "flex",
+                    "flex-direction": "column",
+                    "align-items": "center",
+                    "padding-top": "10%"
+				}}
+			>
+				<button
+					ref={(el: HTMLButtonElement) => (btnRef = el)}
+					style={{
+						width: "10%",
+						height: "100%",
+						"background-color": "black",
+						color: "white",
+					}}
+				>
+					버튼
+				</button>
+			</div>
+			<Dialog 
+            Title="타이틀" 
+            Desc="Desc" 
+            TrgRef={btnRef}
+            />
+		</main>
+	);
 }
