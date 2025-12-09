df18ff2f7c6c7eea3885878fa5d17bf9d504e93c
mangcho3542 <qgeon0419@gmail.com>
2025-12-09 11:50:49 +0900

    Checkbox 및 CheckboxGroup 컴포넌트 완성함.

---
+++ src/components/Checkbox.tsx
@@ -0,0 +1,74 @@
+import styles from "@styles/Checkbox.module.css";
+import {
+	Checkbox as ArkCheckbox,
+	CheckboxRootProps as ArkCheckboxRootProps,
+	CheckboxLabelProps as ArkCheckboxLabelProps,
+	CheckboxControlProps as ArkCheckboxControlProps,
+	CheckboxIndicatorProps as ArkCheckboxIndicatorProps,
+} from "@ark-ui/solid";
+import { PUS } from "@/types/ComponentProps";
+import { JSXElement, splitProps } from "solid-js";
+import { splitComponentProps } from "@/utils/splitComponentProps";
+
+export type CheckboxLabelProps = PUS<ArkCheckboxLabelProps>;
+export type CheckboxControlProps = PUS<ArkCheckboxControlProps>;
+export type CheckboxIndicatorProps = PUS<ArkCheckboxIndicatorProps>;
+
+export interface CheckboxProps extends PUS<ArkCheckboxRootProps> {
+	Label?: JSXElement;
+	LabelProps?: CheckboxLabelProps;
+	ControlProps?: CheckboxControlProps;
+	IndicatorProps?: CheckboxIndicatorProps;
+}
+
+export function Checkbox(props: CheckboxProps) {
+	const [local, rest] = splitProps(props, [
+		"Label",
+		"LabelProps",
+		"ControlProps",
+		"IndicatorProps",
+		"defaultChecked",
+		"invalid",
+		"disabled",
+		"value",
+	]);
+
+	return (
+		<ArkCheckbox.Root
+			{...splitComponentProps(rest, styles.Root)}
+			defaultChecked={
+				local.defaultChecked !== undefined ? local.defaultChecked : false
+			}
+			invalid={local.invalid}
+			disabled={local.disabled}
+			value={local.value}
+		>
+			<ArkCheckbox.Control
+				{...splitComponentProps(local.ControlProps, styles.Control)}
+			>
+				<ArkCheckbox.Indicator
+					{...splitComponentProps(local.IndicatorProps, styles.Indicator)}
+				>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						height="24px"
+						viewBox="0 -960 960 960"
+						width="24px"
+						class={styles.Icon}
+					>
+						<path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z" />
+					</svg>
+				</ArkCheckbox.Indicator>
+			</ArkCheckbox.Control>
+
+			<ArkCheckbox.Label
+				{...splitComponentProps(local.LabelProps, styles.Label)}
+			>
+				{local.Label}
+			</ArkCheckbox.Label>
+			<ArkCheckbox.HiddenInput />
+		</ArkCheckbox.Root>
+	);
+}
+
+export default Checkbox;
---
+++ src/components/CheckboxGroup.tsx
@@ -0,0 +1,68 @@
+import styles from "@styles/Checkbox.module.css";
+import {
+	CheckboxGroupProps as ArkCheckboxGroupProps,
+	Checkbox as ArkCheckbox,
+} from "@ark-ui/solid";
+import { createMemo, For, splitProps } from "solid-js";
+import { CheckboxProps, Checkbox } from "./Checkbox";
+import { PUS } from "@/types/ComponentProps";
+import { splitComponentProps } from "@/utils/splitComponentProps";
+
+export interface CheckboxGroupProps extends PUS<ArkCheckboxGroupProps> {
+	CheckboxAry?: CheckboxProps[];
+	flexDir?: "row" | "column";
+}
+
+export function CheckboxGroup(props: CheckboxGroupProps) {
+	const [local, rest] = splitProps(props, [
+		"CheckboxAry",
+		"name",
+		"value",
+		"defaultValue",
+		"flexDir",
+	]);
+
+	const other = createMemo(() =>
+		splitComponentProps(rest, styles.CheckboxGroup)
+	);
+
+	const getDefaultValue = createMemo(() => {
+		if(local.defaultValue !== undefined) return local.defaultValue;
+		if(local.CheckboxAry == undefined) return undefined;
+		let res: string[] = [];
+		local.CheckboxAry.forEach(({checked, defaultChecked, value}) => {
+			if((checked || defaultChecked) && value) res.push(value);
+		})
+		return res;
+	})
+
+	return (
+		<ArkCheckbox.Group
+			name={local.name}
+			value={local.value}
+			defaultValue={getDefaultValue()}
+			class={other().class}
+			id={other().id}
+			classList={other().classList}
+			style={{ ...other().style, "flex-direction": local.flexDir ?? "column" }}
+		>
+			<For each={local.CheckboxAry}>
+				{(props) => (
+					<Checkbox
+						value={props.value}
+						Label={props.Label}
+						invalid={props.invalid}
+						disabled={props.disabled}
+						defaultChecked={props.defaultChecked}
+						LabelProps={props.LabelProps}
+						ControlProps={props.ControlProps}
+						IndicatorProps={props.IndicatorProps}
+						{...props}
+					/>
+				)}
+			</For>
+		</ArkCheckbox.Group>
+	);
+}
+
+export default CheckboxGroup;
