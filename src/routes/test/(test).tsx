77442795999afb064ee4b97e2fba3c857e3c56bd
mangcho3542 <qgeon0419@gmail.com>
2025-12-09 17:38:14 +0900

    NumberInput 컴포넌트 테스트함.

--- src/routes/test/(test).tsx
+++ src/routes/test/(test).tsx
@@ -1,47 +1,24 @@
 import styles from "./test.module.css";
-import CheckboxGroup from "@/components/CheckboxGroup"
+import NumberInput from "@/components/NumberInput";
 
 export default function test() {
 	return (
 		<main class="Main" style={{
 			"align-items": "center"
 		}}>
-			<CheckboxGroup
-			class={styles.CheckboxGroup}
-			CheckboxAry={[
-				{
-					value: "기본",
-					Label: "기본"
-				},
-				{
-					value: "기본 defaultChecked",
-					Label: "기본 defaultChecked",
-					defaultChecked: true
-				},
-				{
-					value: "invalid",
-					Label: "invalid",
-					invalid: true
-				},
-				{
-					value: "invalid defaultChecked",
-					Label: "invalid defaultChecked",
-					invalid: true,
-					defaultChecked: true
-				},
-				{
-					value: "disabled",
-					Label: "disabled",
-					disabled: true
-				},
-				{
-					value: "disabled defaultChecked",
-					Label: "disabled defaultChecked",
-					disabled: true,
-					defaultChecked: true
-				},
-			]}
-			/>
+			<div
+			style={{
+				display: "flex",
+				"flex-direction": "column",
+				"align-items": "center",
+				padding: "5%"
+			}}
+			>
+				<NumberInput
+				Label="Label"
+				useTrg={true}
+				/>
+			</div>
 		</main>
 	)
 }
