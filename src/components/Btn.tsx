f0067b0608acab22e956bfe87b24da738147d0a4
mangcho3542 <qgeon0419@gmail.com>
2025-12-15 22:24:10 +0900

    Btn.tsx에서 Btn컴포넌트를 export와 default export 모두 하도록 수정함.

--- src/components/Btn.tsx
+++ src/components/Btn.tsx
@@ -5,8 +5,10 @@ import styles from "@styles/Btn.module.css";
 
 export type BtnProps = PUS<ButtonProps>;
 
-export default function Btn(props: BtnProps): JSX.Element {
+export function Btn(props: BtnProps): JSX.Element {
   const [local, rest] = splitProps(props, ["children"]);
 
   return <button {...splitComponentProps(rest, styles.Btn)}>{local.children}</button>;
 }
+
+export default Btn;
