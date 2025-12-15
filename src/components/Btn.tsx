88f1365f4b62f7b152c586f193e8c649af962896
mangcho3542 <qgeon0419@gmail.com>
2025-12-15 15:12:42 +0900

    BtnProps 관련하여 Btn.tsx 수정함.

--- src/components/Btn.tsx
+++ src/components/Btn.tsx
@@ -1,12 +1,9 @@
-import { PUS } from "@/types/ComponentProps";
+import { PUS, ButtonProps } from "@/types/ComponentProps";
 import { splitProps, JSX } from "solid-js";
-import { ButtonProps } from "@/types/ComponentProps";
 import { splitComponentProps } from "@/utils/splitComponentProps";
 import styles from "@styles/Btn.module.css";
 
-export interface BtnProps extends PUS<ButtonProps> {
-  ref?: HTMLButtonElement | ((el: HTMLButtonElement) => void);
-}
+export type BtnProps = PUS<ButtonProps>;
 
 export default function Btn(props: BtnProps): JSX.Element {
   const [local, rest] = splitProps(props, ["children"]);
