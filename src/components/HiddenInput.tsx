99fbb1a444a86902d95d39fe0b74c22aa1af3a41
mangcho3542 <qgeon0419@gmail.com>
2025-12-15 22:26:40 +0900

    HiddenInput 컴포넌트 민듬.

---
+++ src/components/HiddenInput.tsx
@@ -0,0 +1,23 @@
+import { InputProps } from "@/types/ComponentProps";
+import { splitProps } from "solid-js";
+
+export interface HiddenInputProps extends InputProps {}
+
+export function HiddenInput(props: HiddenInputProps) {
+	const [local, rest] = splitProps(props, ["style", "type"]);
+
+	return (
+		<input
+			style={{
+				...local.style,
+				visibility: "hidden",
+				display: "none"
+			}}
+			type={local.type ?? "text"}
+			aria-hidden="true"
+			{...rest}
+		/>
+	);
+}
+
+export default HiddenInput;
