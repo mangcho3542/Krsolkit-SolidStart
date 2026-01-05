4de812c906e6874ac06780b324dc7cac8cf32014
mangcho3542 <qgeon0419@gmail.com>
2026-01-05 22:05:41 +0900

    Highlight 컴포넌트도 ark-ui안쓰도록 수정함.

--- src/components/Highlight.tsx
+++ src/components/Highlight.tsx
@@ -1,23 +1,67 @@
-import {
-  Highlight as ArkHighlight,
-  HighlightProps as ArkHighlightProps,
-} from "@ark-ui/solid";
-import { splitProps } from "solid-js";
+import styles from "@styles/Highlight.module.css";
+import { PUS, SpanProps } from "@types";
+import { splitComponentProps } from "@utils";
+import { For, splitProps } from "solid-js";
 
-export interface HighlightProps extends Omit<ArkHighlightProps, "text"> {
-  children: string;
+export interface HighlightProps extends PUS<SpanProps> {
+	children: string;
+	query?: string | string[];
+	QueryProps?: PUS<SpanProps>;
+	color?: string;
+	bgColor?: string;
 }
 
 export function Highlight(props: HighlightProps) {
-  const [local, rest] = splitProps(props, ["query", "children"]);
+	const [local, others, rest] = splitProps(
+		props,
+		["query", "children", "color", "bgColor"],
+		["QueryProps"]
+	);
 
-  return (
-    <ArkHighlight
-      query={local.query}
-      text={local.children}
-      {...rest}
-    />
-  );
+	function display(str: string) {
+		if (!local.query) return <>{str}</>;
+
+		if (typeof local.query === "string") {
+			if (str === local.query)
+				return (
+					<span
+						{...splitComponentProps(others.QueryProps, styles.Query)}
+						data-scope={others.QueryProps?.["data-scope"] ?? "highlight"}
+						data-part={others.QueryProps?.["data-part"] ?? "query"}
+					>
+						{str}
+					</span>
+				);
+		} else {
+			if (local.query.includes(str))
+				return (
+					<span
+						{...splitComponentProps(others.QueryProps, styles.Query)}
+						data-scope={others.QueryProps?.["data-scope"] ?? "highlight"}
+						data-part={others.QueryProps?.["data-part"] ?? "query"}
+					>
+						{str}
+					</span>
+				);
+		}
+
+		return <>{str + " "}</>;
+	}
+
+	return (
+		<span
+			{...splitComponentProps(rest, styles.Root)}
+			data-scope={rest["data-scope"] ?? "highlight"}
+			data-part={rest["data-part"] ?? "root"}
+      style={{
+        ...rest.style,
+        "--color": local.color ?? "#54abf3",
+        "--bg-color": local.bgColor ?? "#cff9ff"
+      }}
+		>
+			<For each={local.children.split(" ")}>{(str) => display(str)}</For>
+		</span>
+	);
 }
 
 export default Highlight;
