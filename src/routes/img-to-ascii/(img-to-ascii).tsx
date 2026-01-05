0424a7d0aed9efbc4daf362392c3fed9e13c30e7
mangcho3542 <qgeon0419@gmail.com>
2026-01-05 18:08:18 +0900

    img-to-ascii 페이지 완성함.

---
+++ src/routes/img-to-ascii/(img-to-ascii).tsx
@@ -0,0 +1,18 @@
+import { Meta, Title } from "@solidjs/meta";
+import Main from "./Main";
+
+export default function imgToAscii() {
+
+	return (
+		<>
+			<Title>아스키 변환</Title>
+			<Meta
+				name="description"
+				content="이미지를 업로드하면 아스키로 변환하는 기능을 제공합니다."
+			/>
+			<Meta name="keywords" content="아스키 아트, 이미지 변환, 아스키 코드" />
+			<Meta property="og:title" content="Krsorkit - 아스키 변환" />
+			<Main />
+		</>
+	);
+}
