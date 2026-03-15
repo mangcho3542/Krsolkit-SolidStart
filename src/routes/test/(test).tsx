151a11f1bae98382d5dfc40ca1c27cf1a843070b
mangcho3542 <qgeon0419@gmail.com>
2026-03-15 14:48:34 +0900

    Toast컴포넌트 테스트함.

--- src/routes/test/(test).tsx
+++ src/routes/test/(test).tsx
@@ -1,22 +1,83 @@
-import { Dialog } from "@components";
-import { createSignal } from "solid-js";
+import { Toast, createToaster } from "@components/Toast";
 
-export default function test() {
-	const [trgRef, setTrgRef] = createSignal<HTMLButtonElement | undefined>();
+interface I {
+	top: number;
+	bottom: number;
+	left: number;
+	right: number;
+	width: number;
+	height: number;
+	x: number;
+	y: number;
+}
+
+interface I2 {
+	top: boolean;
+	bottom: boolean;
+	left: boolean;
+	right: boolean;
+	width: boolean;
+	height: boolean;
+	x: boolean;
+	y: boolean;
+}
+
+export default function Test() {
+	const toaster = createToaster();
+	let btnRef: HTMLButtonElement | undefined;
+	let map: Map<string, object> = new Map<string, object>();
+
+	function trackPosition() {
+		const id = setInterval(() => {
+			const btn = btnRef;
+			if (!btn) return;
+			const rect = btn.getBoundingClientRect();
+			const { top, bottom, left, right, width, height, x, y } = rect;
+			map.set(
+				JSON.stringify({ top, bottom, left, right, width, height, x, y }),
+				{
+					top,
+					bottom,
+					left,
+					right,
+					width,
+					height,
+					x,
+					y,
+				},
+			);
+
+			if (x === 0) {
+				clearInterval(id);
+				for (const v of map.values()) {
+					console.log(v);
+				}
+			}
+		}, 10);
+
+		return id;
+	}
+
+	trackPosition();
 
 	return (
-		<main class="Main" style={{
-			"align-items": "center"
-		}}>
-			<button ref={(el) => {setTrgRef(el);}}>
+		<main class="Main items-center">
+			<button
+				class="bg-black text-white p-[1%] rounded-md"
+				onClick={() => {
+					toaster.create({ title: "title" });
+				}}
+			>
 				버튼
 			</button>
-
-			<Dialog TrgRef={trgRef} 
-			style={{
-				"height": "100%"
-			}}
+			<Toast
+				toaster={toaster}
+				CloseBtnProps={{
+					ref: (el) => {
+						btnRef = el;
+					},
+				}}
 			/>
 		</main>
-	)
+	);
 }
