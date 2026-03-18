fd14f7190cd52a9d90628da221bfe40aded8c6ea
mangcho3542 <qgeon0419@gmail.com>
2026-03-19 00:02:03 +0900

    serverApi 및 clientApi에서 axios import하는 위치를 redaxios로 수정함.

--- src/lib/serverApi.ts
+++ src/lib/serverApi.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from "redaxios";
 
 export const serverApi = axios.create({
 	baseURL: process.env.SERVER_URL,
--- src/utils/clientApi.ts
+++ src/utils/clientApi.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from "redaxios";
 
 export const clientApi = axios.create({
     baseURL: "/api",
