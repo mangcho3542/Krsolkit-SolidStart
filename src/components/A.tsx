3cafb9869c2f850118ec6e0df759aad0da21299a
mangcho3542 <qgeon0419@gmail.com>
2026-03-23 17:13:34 +0900

    A컴포넌트를 tailwind방식으로 바꿈.

--- src/components/A.tsx
+++ src/components/A.tsx
@@ -1,38 +1,40 @@
 import { A as Anchor, AnchorProps } from "@solidjs/router";
-import styles from "@styles/A.module.css";
 import { splitProps } from "solid-js";
+import { twMerge } from "tailwind-merge";
 
-export interface AProps extends AnchorProps {
+export interface AProps extends Omit<AnchorProps, "ClassList"> {
 	href: string;
-	useDefaultStyle?: boolean;
 }
 
 export function A(props: AProps) {
 	const [local, rest] = splitProps(props, [
-		"href",
 		"target",
 		"children",
-		"useDefaultStyle",
-		"class",
-		"classList",
-		"id",
-		"inactiveClass",
-		"activeClass",
 	]);
 
 	return (
 		<Anchor
-			class={local.class}
-			classList={{
-				[styles.A]:
-					local.useDefaultStyle === undefined ? true : local.useDefaultStyle,
-			}}
-			id={local.id}
-			target={local.target ?? "_self"}
-			href={local.href}
-			inactiveClass={local.inactiveClass}
-			activeClass={local.activeClass}
 			{...rest}
+			class={twMerge(
+				"no-underline inline-block text-inherit relative box-border",
+				"after:absolute",
+				"after:bottom-0",
+				"after:left-0",
+				"after:content-none",
+				"after:w-full",
+				"after:border-b-2",
+				"after:border-solid",
+				"after:border-[#bdbdbd]",
+				"after:h-px",
+				"after:-translate-y-[0.2rem]",
+				"after:opacity-0",
+				"after:transition-all",
+				"duration-500",
+				"hover:after:opacity-100",
+				"hover:after:translate-y-[0.1rem]",
+				rest.class,
+			)}
+			target={local.target ?? "_self"}
 		>
 			{local.children}
 		</Anchor>
